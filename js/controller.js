a59c9f2ddd954de0f7afaee4bf1b2363af9c65e6
Daniel Almeida <danielf.almeida@gmail.com>
2014-12-24 00:30:01 +0000

    Camel Case & records in an object
    records are now stored in an object with the the names using the
    corresponding header title in camel case.

--- js/controller.js
+++ js/controller.js
@@ -13,7 +13,14 @@ String.prototype.splitCSVLine = function() {
   return this.split(/[,;\t]+/);
 }
 
-
+/**
+ * method to calculate the camelCase of a string
+ */
+String.prototype.camelCase = function() {
+  return this.toLowerCase().replace(/[-\s](.)/g, function(match, group1) {
+    return group1.toUpperCase();
+  });
+}
 
 
 var mapQuestApp = angular.module('mapQuestApp', []);
@@ -31,6 +38,25 @@ mapQuestApp.controller('MapQuestCtrl', function($scope) {
 
 
 
+  /**
+   * generates an object from the data of the splitted csv row and uses
+   * the corresponding column header value as it's name
+   */
+  $scope.generateRecordRowFromCSVArray = function(csvArray) {
+    if (typeof $scope.csvData.headers === 'undefined' || $scope.csvData.headers == null) return {};
+
+    var returningObj = {};
+
+    for (var i in csvArray) {
+      if (typeof $scope.csvData.headers[i]!=='undefined') {
+        var columnName = $scope.csvData.headers[i].camelCase();
+        returningObj[columnName] = csvArray[i];
+      }
+
+    }
+
+    return returningObj
+  }
 
 
 
@@ -64,7 +90,11 @@ mapQuestApp.controller('MapQuestCtrl', function($scope) {
       }
 
       if (count == 0) $scope.csvData.headers = splittedCsvRow;
-      else $scope.csvData.records.push(splittedCsvRow);
+      else {
+        //$scope.csvData.records.push(splittedCsvRow);
+        $scope.csvData.records.push($scope.generateRecordRowFromCSVArray(splittedCsvRow));
+
+      }
 
       count++;
     }
